6b5a44ac46705720fa5f0a14ee6a0cf472a9d574
Patrik Huggenberger <huggi.15@gmail.com>
2016-05-25 23:58:55 +0200

    first dashboard layout draft

--- app/src/main/java/ch/fhnw/ip6/powerconsumptionmanager/util/DashboardHelper.java
+++ app/src/main/java/ch/fhnw/ip6/powerconsumptionmanager/util/DashboardHelper.java
@@ -1,30 +1,88 @@
 package ch.fhnw.ip6.powerconsumptionmanager.util;
 
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.gigamole.library.ArcProgressStackView;
-import com.gigamole.library.ArcProgressStackView.*;
+import com.gigamole.library.ArcProgressStackView.IndicatorOrientation;
+import com.gigamole.library.ArcProgressStackView.Model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * Created by Patrik on 15.05.2016.
- */
+import ch.fhnw.ip6.powerconsumptionmanager.R;
+
 public class DashboardHelper {
-    private HashMap<String, ArcProgressStackView> mArcProgressViews;
+    private static final AtomicInteger GENERATED_ID = new AtomicInteger(1);
+    private static final HashMap<String, Integer> GENERATED_ARCSV_IDS = new HashMap<>();
+    private static final HashMap<String, Integer> GENERATED_POWER_IDS = new HashMap<>();
+    private static final HashMap<String, Integer> GENERATED_VALUECONTAINER_IDS = new HashMap<>();
 
-    public DashboardHelper() {
-        mArcProgressViews = new HashMap<>();
+    private HashMap<String, ArcProgressStackView> mComponentViews;
+    private HashMap<String, ProgressBar> mVerticalProgressbars;
+    private LinearLayout mLinearLayout;
+    private Context mContext;
+    private float mDensity;
+
+    public DashboardHelper(Context c, LinearLayout ll) {
+        mContext = c;
+        mLinearLayout = ll;
+        mComponentViews = new HashMap<>();
+        mVerticalProgressbars = new HashMap<>();
+        mDensity = mContext.getResources().getDisplayMetrics().density;
     }
 
-    public void addArcProgressStackView(String key, ArcProgressStackView apsv) {
-        mArcProgressViews.put(key, apsv);
+    public HashMap<String, Integer> getArcsvIdsMap() {
+        return GENERATED_ARCSV_IDS;
     }
 
-    public void setModel(String key, ArrayList<Model> apsvModels) {
-        mArcProgressViews.get(key).setModels(apsvModels);
+    public float getDensity() {
+        return mDensity;
     }
 
-    public ArrayList<Model> generateModel(String description, int progress, int bgProgressColor, int progressColor) {
+    public void addVerticalProgressbar(String key, ProgressBar pb) { mVerticalProgressbars.put(key, pb); }
+
+    public void setVerticalProgress(String key, int progress) {
+        ProgressBar pb = mVerticalProgressbars.get(key);
+        double lowerMiddle = pb.getMax()/2 - pb.getMax() * 0.05;
+        double upperMiddle = pb.getMax()/2 + pb.getMax() * 0.05;
+
+        if(key.equals("Bezug")) {
+            if(progress <= lowerMiddle) {
+                pb.setProgressDrawable(ContextCompat.getDrawable(mContext, R.drawable.progressbar_vertical_positive));
+            } else if(progress > lowerMiddle && progress < upperMiddle) {
+                pb.setProgressDrawable(ContextCompat.getDrawable(mContext, R.drawable.progressbar_vertical_neutral));
+            } else {
+                pb.setProgressDrawable(ContextCompat.getDrawable(mContext, R.drawable.progressbar_vertical_negative));
+            }
+        }
+
+        pb.setProgress(progress);
+    }
+
+    public void addComponentView(String key, ArcProgressStackView apsv) {
+        mComponentViews.put(key, apsv);
+    }
+
+    public void setPowerForComponent(String key, ArrayList<Model> apsvModels) {
+        mComponentViews.get(key).setModels(apsvModels);
+    }
+
+    public void updatePowerForComponent(String key, int power) {
+        for(ArcProgressStackView.Model model : mComponentViews.get(key).getModels()) {
+            model.setProgress(power);
+        }
+    }
+
+    public ArrayList<Model> generateModelForComponent(String description, int progress, int bgProgressColor, int progressColor) {
         Model m = new Model(description, progress, bgProgressColor, progressColor);
         ArrayList<Model> models = new ArrayList<>();
         models.add(m);
@@ -32,14 +90,137 @@ public class DashboardHelper {
     }
 
     public void displayNonAnimated() {
-        for (ArcProgressStackView apsv : mArcProgressViews.values()) {
+        for (ArcProgressStackView apsv : mComponentViews.values()) {
             apsv.invalidate();
         }
     }
 
     public void displayAnimated() {
-        for (ArcProgressStackView apsv : mArcProgressViews.values()) {
+        for (ArcProgressStackView apsv : mComponentViews.values()) {
             apsv.animateProgress();
         }
+    }
+
+    public void generateDynamicComponentsLayout(String componentId, int color) {
+        // RelativeLayout container
+        RelativeLayout rlContainer = new RelativeLayout(mContext);
+        LinearLayout.LayoutParams rlContainerLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        rlContainer.setLayoutParams(rlContainerLayoutParams);
+
+        // ArcProgressStackView
+        ArcProgressStackView arcsv = new ArcProgressStackView(mContext);
+        arcsv.setId(handleNewId(componentId + "arcsvId", GENERATED_ARCSV_IDS));
+        arcsv.setIsShadowed(true);
+        arcsv.setShadowDistance(1);
+        arcsv.setShadowRadius(2);
+        arcsv.setIsAnimated(true);
+        arcsv.setAnimationDuration(2000);
+        arcsv.setIsDragged(false);
+        arcsv.setTextColor(color);
+        arcsv.setDrawWidthFraction((float) 0.15);
+        arcsv.setModelBgEnabled(true);
+        arcsv.setStartAngle(135);
+        arcsv.setSweepAngle(270);
+        arcsv.setIndicatorOrientation(IndicatorOrientation.HORIZONTAL);
+        this.addComponentView(componentId, arcsv);
+        this.setPowerForComponent(
+            componentId,
+            this.generateModelForComponent(
+                "",
+                30, // TODO Progress
+                ContextCompat.getColor(mContext, R.color.colorArcBackground),
+                color
+            )
+        );
+
+        /**
+         * LayoutParams for the ArcProgressStackView is set after layout dimensions for parent container have been set.
+         * See setupViewTreeObserver in OverviewFragment.
+         */
+
+        rlContainer.addView(arcsv);
+
+        // LinearLayout for values
+        LinearLayout llValueContainer = new LinearLayout(mContext);
+        llValueContainer.setId(handleNewId(componentId + "llValueContainerId", GENERATED_VALUECONTAINER_IDS));
+        llValueContainer.setOrientation(LinearLayout.HORIZONTAL);
+        llValueContainer.setGravity(Gravity.CENTER);
+
+        RelativeLayout.LayoutParams llValueContainerLayoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        llValueContainerLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        llValueContainer.setLayoutParams(llValueContainerLayoutParams);
+
+        // Textviews for values
+        TextView tvValue = new TextView(mContext);
+        tvValue.setId(handleNewId(componentId + "powerId", GENERATED_POWER_IDS));
+        tvValue.setText("5.6");
+        tvValue.setTextSize(25);
+        tvValue.setTextColor(ContextCompat.getColor(mContext, R.color.colorTextPrimary));
+
+        TextView tvValueMax = new TextView(mContext);
+        tvValueMax.setText("/6.0 kW");
+        tvValueMax.setTextSize(10);
+        tvValueMax.setTextColor(ContextCompat.getColor(mContext, R.color.colorTextPrimary));
+
+        LinearLayout.LayoutParams tvValueLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        tvValueLayoutParams.gravity = Gravity.BOTTOM;
+
+        tvValue.setLayoutParams(tvValueLayoutParams);
+        tvValueMax.setLayoutParams(tvValueLayoutParams);
+
+        llValueContainer.addView(tvValue);
+        llValueContainer.addView(tvValueMax);
+
+        rlContainer.addView(llValueContainer);
+
+        // Textview component description
+        TextView tvComponent = new TextView(mContext);
+        tvComponent.setText(componentId);
+        tvComponent.setTextSize(14);
+        tvComponent.setTextColor(ContextCompat.getColor(mContext, R.color.colorTextPrimary));
+
+        RelativeLayout.LayoutParams tvComponentLayoutParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        tvComponentLayoutParams.setMargins(0, (int) mDensity * 35, 0, 0);
+        tvComponentLayoutParams.addRule(RelativeLayout.BELOW, GENERATED_VALUECONTAINER_IDS.get(componentId + "llValueContainerId"));
+        tvComponentLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+        tvComponent.setLayoutParams(tvComponentLayoutParams);
+
+        rlContainer.addView(tvComponent);
+
+        mLinearLayout.addView(rlContainer);
+    }
+
+    // Implementation from google
+    public static int generateViewId() {
+        for (;;) {
+            final int result = GENERATED_ID.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (GENERATED_ID.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
+    }
+
+    private int handleNewId(String key, HashMap<String, Integer> map) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            map.put(key, generateViewId());
+        } else {
+            map.put(key, View.generateViewId());
+        }
+        return map.get(key);
     }
 }
--- app/src/main/java/ch/fhnw/ip6/powerconsumptionmanager/view/OverviewFragment.java
+++ app/src/main/java/ch/fhnw/ip6/powerconsumptionmanager/view/OverviewFragment.java
@@ -1,20 +1,18 @@
 package ch.fhnw.ip6.powerconsumptionmanager.view;
 
-import android.content.Context;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.LayerDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 import com.gigamole.library.ArcProgressStackView;
 
@@ -24,8 +22,12 @@ import ch.fhnw.ip6.powerconsumptionmanager.util.DashboardHelper;
 public class OverviewFragment extends Fragment {
     private final static String PRODUCER_NAME = "Photovoltaik";
     private final static String CONSUMER_NAME = "VerbrauchTot";
+    private final static String AUTARCHY = "Autarkie";
+    private final static String POWERCONSUMPTION = "Eigenverbrauch";
+    private final static String RECEIPT = "Bezug";
 
     private DashboardHelper mDashBoardHelper;
+    private Handler mUpdateHandler;
 
     public static OverviewFragment newInstance() {
         return new OverviewFragment();
@@ -33,7 +35,9 @@ public class OverviewFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_overview, container, false);
+        View view = inflater.inflate(R.layout.fragment_overview, container, false);
+        setupViewTreeObserver(view);
+        return view;
     }
 
     @Override
@@ -42,41 +46,21 @@ public class OverviewFragment extends Fragment {
 
         final int[] graphColors = getContext().getResources().getIntArray(R.array.colorsGraph);
 
-        mDashBoardHelper = new DashboardHelper();
-        mDashBoardHelper.addArcProgressStackView(PRODUCER_NAME, (ArcProgressStackView) getView().findViewById(R.id.arcProgressProducer));
-        mDashBoardHelper.addArcProgressStackView(CONSUMER_NAME, (ArcProgressStackView) getView().findViewById(R.id.arcProgressConsumer));
+        mDashBoardHelper = new DashboardHelper(getContext(), (LinearLayout) getView().findViewById(R.id.llDynamicContent));
+        mDashBoardHelper.addComponentView(PRODUCER_NAME, (ArcProgressStackView) getView().findViewById(R.id.arcProgressProducer));
+        mDashBoardHelper.addComponentView(CONSUMER_NAME, (ArcProgressStackView) getView().findViewById(R.id.arcProgressConsumer));
+        mDashBoardHelper.addVerticalProgressbar(AUTARCHY, (ProgressBar) getView().findViewById(R.id.vProgressAutarchy));
+        mDashBoardHelper.addVerticalProgressbar(RECEIPT, (ProgressBar) getView().findViewById(R.id.vProgressReceipt));
+        mDashBoardHelper.addVerticalProgressbar(POWERCONSUMPTION, (ProgressBar) getView().findViewById(R.id.vProgressPowerConsumption));
 
-        ProgressBar pb = (ProgressBar) getView().findViewById(R.id.vProgressReceipt);
-        int progress = (int) Math.floor(Math.random() * 41);
-        if(progress <= 17) {
-            pb.setProgressDrawable(ContextCompat.getDrawable(getContext(), R.drawable.progressbar_vertical_positive));
-        } else if(progress > 17 && progress < 23) {
-            pb.setProgressDrawable(ContextCompat.getDrawable(getContext(), R.drawable.progressbar_vertical_neutral));
-        } else {
-            pb.setProgressDrawable(ContextCompat.getDrawable(getContext(), R.drawable.progressbar_vertical_negative));
-        }
-        pb.setProgress(progress);
+        //Dynamisch
+        mDashBoardHelper.generateDynamicComponentsLayout("Boiler", ContextCompat.getColor(getContext(), R.color.colorDynamicConsumer1));
+        mDashBoardHelper.generateDynamicComponentsLayout("Wärmepumpe", ContextCompat.getColor(getContext(), R.color.colorDynamicConsumer2));
+        mDashBoardHelper.generateDynamicComponentsLayout("Emobil", ContextCompat.getColor(getContext(), R.color.colorDynamicConsumer3));
 
-//        FrameLayout container = (FrameLayout) getView().findViewById(R.id.frame);
-//        ArcProgressStackView arcsv = new ArcProgressStackView(getContext());
-//        arcsv.setStartAngle(135);
-//        arcsv.setSweepAngle(270);
-//        mDashBoardHelper.addArcProgressStackView("Test", arcsv);
-//        mDashBoardHelper.setModel(
-//            "Test",
-//            mDashBoardHelper.generateModel(
-//                "",
-//                100,
-//                ContextCompat.getColor(getContext(), R.color.colorArcBackground),
-//                graphColors[2]
-//            )
-//        );
-//        container.addView(arcsv);
-
-
-        mDashBoardHelper.setModel(
+        mDashBoardHelper.setPowerForComponent(
             PRODUCER_NAME,
-            mDashBoardHelper.generateModel(
+            mDashBoardHelper.generateModelForComponent(
                 "",
                 (int) Math.floor(Math.random() * 101),
                 ContextCompat.getColor(getContext(), R.color.colorArcBackground),
@@ -84,9 +68,9 @@ public class OverviewFragment extends Fragment {
             )
         );
 
-        mDashBoardHelper.setModel(
+        mDashBoardHelper.setPowerForComponent(
                 CONSUMER_NAME,
-                mDashBoardHelper.generateModel(
+                mDashBoardHelper.generateModelForComponent(
                         "",
                         (int) Math.floor(Math.random() * 101),
                         ContextCompat.getColor(getContext(), R.color.colorArcBackground),
@@ -94,6 +78,58 @@ public class OverviewFragment extends Fragment {
                 )
         );
 
+        mDashBoardHelper.setVerticalProgress(AUTARCHY, (int) Math.floor(Math.random() * 101));
+        mDashBoardHelper.setVerticalProgress(RECEIPT, (int) Math.floor(Math.random() * 41));
+        mDashBoardHelper.setVerticalProgress(POWERCONSUMPTION, (int) Math.floor(Math.random() * 101));
+
         mDashBoardHelper.displayAnimated();
+
+        mUpdateHandler = new Handler();
+        mUpdateHandler.postDelayed(updateData, 5000);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Stop data updater if instantiated
+        if(mUpdateHandler != null) {
+            mUpdateHandler.removeCallbacks(updateData);
+        }
+    }
+
+    private final Runnable updateData = new Runnable() {
+        public void run() {
+            mDashBoardHelper.updatePowerForComponent(CONSUMER_NAME, (int) Math.floor(Math.random() * 101));
+            mDashBoardHelper.displayAnimated();
+            mUpdateHandler.postDelayed(this, 5000);
+        }
+    };
+
+    private void setupViewTreeObserver(View view) {
+        final LinearLayout llDynamicContent = (LinearLayout) view.findViewById(R.id.llDynamicContent);
+        ViewTreeObserver vto = llDynamicContent.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < 16) {
+                    llDynamicContent.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    llDynamicContent.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+
+                RelativeLayout.LayoutParams arcsvLayoutParams = new RelativeLayout.LayoutParams(
+                        llDynamicContent.getWidth(),
+                        llDynamicContent.getHeight()
+                );
+                arcsvLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                int margins = (int) mDashBoardHelper.getDensity() * 8;
+                arcsvLayoutParams.setMargins(margins, margins, margins, margins);
+
+                for (int id: mDashBoardHelper.getArcsvIdsMap().values()) {
+                    ArcProgressStackView arcsv = (ArcProgressStackView) getView().findViewById(id);
+                    arcsv.setLayoutParams(arcsvLayoutParams);
+                }
+            }
+        });
     }
 }
